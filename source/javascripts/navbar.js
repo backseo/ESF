dae27ce1c9a8105af6f58e3d10ea68502128275f
serge cleoron <sc21417@gmail.com>
2021-09-01 16:04:08 +0200

    navbar mouvement & offset top all pages

---
+++ source/javascripts/navbar.js
@@ -0,0 +1,25 @@
+// const navbar = document.querySelector('.nav');
+// let sticky = navbar.offsetTop;
+// const navbarScroll = () => {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add('sticky');
+//   } else {
+//     navbar.classList.remove('sticky');
+//   }
+// };
+
+// window.onScroll = navbarScroll;
+
+
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+      $('.navbar-name').css("height", 50);
+      $('#logo').css("height", 46);
+      $('#logo').css("width", 167);
+    }
+    else {
+      $('.navbar-name').css("height", 130);
+      $('#logo').css("height", 110);
+      $('#logo').css("width", 350);
+    }
+  });
