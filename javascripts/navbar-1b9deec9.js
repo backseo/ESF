f8632cc9a7897790bfb34d2f583d51606ca64059
sc21417@gmail.com <sc21417@gmail.com>
2021-09-08 16:45:54 +0200

    Automated commit at 2021-09-08 14:45:53 UTC by middleman-deploy 1.0.0

---
+++ javascripts/navbar-1b9deec9.js
@@ -0,0 +1,1 @@
+$(window).scroll(function(){$(document).scrollTop()>50?($(".navbar-name").css("height",50),$("#logo").css("height",46),$("#logo").css("width",167),$("#bannertext").css("opacity",0)):($(".navbar-name").css("height",130),$("#logo").css("height",110),$("#logo").css("width",350),$("#bannertext").css("opacity",1))});
